bebd6fe884c00fe25756058c7aa455a92b89a76a
4929433 <linxuan.ye@student.uq.edu.au>
2025-05-13 00:25:42 +1000

    修改1

--- js/link.js
+++ js/link.js
@@ -5,3 +5,10 @@ document.querySelectorAll('.nav a').forEach(link => {
     link.classList.add('active');
   });
 });
+
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('navLinks');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
